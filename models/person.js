99b24e695cd3e8159e50007de0690b5215ef5223
Paul Aderoju <paul.adeboye@yahoo.com>
2024-05-21 17:09:32 +0100

    Exercises 3.19 - 3.21

--- models/person.js
+++ models/person.js
@@ -21,10 +21,44 @@ mongoose.connect(url)
     })
 
 // Person Schema
+// const personSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         minLength: 3,
+//         required: true
+//     },
+//     number: {
+//         type: String,
+//         validate: {
+//             validator: function(v) {
+//                 return /\d{3}-\d{}.test(v);
+//             },
+//             message: props => `${props.value} is not a valid phone number!`
+//         },
+//         required: true
+//     }
+// })
+
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Person', personSchema);
+
 
 // Modify personSchema
 personSchema.set('toJSON', {
